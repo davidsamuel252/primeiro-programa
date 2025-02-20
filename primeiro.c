4db2089b47687ec2f44d49565c545ccfa50516f5
davidsamuel252 <david.becker.silva@escola.pr.gov.br>
2025-02-20 12:10:06 -0300

    primeiro

---
+++ primeiro.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int paulo;
+    int jose;
+    int soma;
+    int multiplicacao;
+    int divisao;
+    paulo = 854;
+    jose  =  24;
+    soma = paulo + jose;
+    multiplicacao = paulo * jose;
+    divisao = paulo / jose;
+    // Write C code here
+    printf("o valor de paulo é %d \n", paulo);
+    printf("o valor de jose é %d \n", jose);
+    printf("o resultado da soma é %d \n", soma);
+    printf("o resultado da multiplicacao é %d \n", multiplicacao);
+    printf("o resultado da divisao é %d", divisao);
+
+    return 0;
+}
